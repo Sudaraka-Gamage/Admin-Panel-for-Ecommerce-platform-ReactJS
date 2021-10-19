36929b20973a6f0f403b42dc5cd3dc1825d16b5d
Sudaraka Gamage <ashenchamli220@gmail.com>
2021-10-19 22:27:47 +0530

    Home page Chart Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import "./home.css"
+import { userData } from '../../dummyData'
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} title = "User Analytics" grid dataKey="Active User"/>
+
         </div>
     )
 }
