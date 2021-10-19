6124f5f9f9dcfc16d4cbb2c5a4be5605680aded5
Sudaraka Gamage <ashenchamli220@gmail.com>
2021-10-19 16:30:10 +0530

    Home page Freatured information

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./featuredInfo.css"
+import {ArrowDownward, ArrowUpward } from '@mui/icons-material';
+
+export default function FeaturedInfo() {
+    return (
+        <div className="featured">
+
+            <div className="featuredItem">
+                <span className="featuredTitle">Revanue</span>
+
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,415</span>
+                    <span className="featuredMoneyRate">
+                        -11.4
+                        <ArrowDownward className="featuredIcon"/>
+                    </span>
+                </div>
+
+                <span className="featuredSub">Compaired To Last Month</span>
+            </div>
+
+
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$4,415</span>
+                    <span className="featuredMoneyRate">
+                        -1.4
+                        <ArrowDownward className="featuredIcon"/>
+                    </span>
+                </div>
+
+                <span className="featuredSub">Compaired To Last Month</span>
+            </div>
+
+
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,225</span>
+                    <span className="featuredMoneyRate">
+                        +2.4
+                        <ArrowUpward className="featuredIcon Upward"/>
+                    </span>
+                </div>
+
+                <span className="featuredSub">Compaired To Last Month</span>
+            </div>
+            
+        </div>
+    );
+}
