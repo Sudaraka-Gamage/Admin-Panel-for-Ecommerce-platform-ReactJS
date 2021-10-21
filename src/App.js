1024daece6939aeff325c1ed3742432a732c193a
Sudaraka Gamage <ashenchamli220@gmail.com>
2021-10-21 16:43:43 +0530

    User Details Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./app.css"
 import Home from "./pages/home/Home";
 import {BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 
 function App() {
@@ -19,15 +20,15 @@ function App() {
                   <Switch>
 
                     <Route exact path="/">
-
                       <Home/>
-
                     </Route>
 
                     <Route path="/users">
-
                       <UserList/>
+                    </Route>
 
+                    <Route path="/user/:userId">
+                     <User/>
                     </Route>
 
                   </Switch>
